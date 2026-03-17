2762277e991d42c918207861c623c3a37e35642d
Cyryl Bitangcol <cyryl.bitangcol@gmail.com>
2026-03-17 09:23:32 +0800

    Fix Website CSS

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  plugins: [
+    tailwindcss(),
+  ],
   build: {
     rollupOptions: {
       input: {
